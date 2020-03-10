cbeb141ff519311cd69dfae5102c925bc594b576
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-03-10 19:59:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function matchName() {
+  
+}
+
+function fuzzyMatch() {
+  
+}
+
+function findMatching(drivers, name) {
+  return drivers.filter(function(d){
+    return d.toUpperCase() === name.toUpperCase()
+  })
+}
